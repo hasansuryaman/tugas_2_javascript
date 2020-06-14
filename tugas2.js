4af08509f770f40305d0314e8109b003f02fcb7b
hasansuryaman <hasan.suryaman@outlook.com>
2020-06-14 21:02:40 +0700

    Tugas 2 Javascript

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+nuumber = 2020
+sttring = 'Juni'
+booolean = true
+nuull = null;
+var unndefined;
+arrray = [3,17,14,6,20,21]
+
+console.log(nuumber)
+console.log(sttring)
+console.log(booolean)
+console.log(nuull)
+console.log(unndefined)
+console.log(arrray)
